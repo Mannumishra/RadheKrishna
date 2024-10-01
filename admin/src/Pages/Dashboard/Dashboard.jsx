aca93ca945608ae8d87b755169e7ad8084ffc947
mannumishra <mannumishra2207@outlook.com>
2024-10-01 18:18:27 +0530

    add changes

--- admin/src/Pages/Dashboard/Dashboard.jsx
+++ admin/src/Pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ const Dashboard = () => {
   return (
     <>
       <h3 style={{ display: "flex", justifyContent: "center", alignItems: "center", margin: "0 auto", }}>
-        Welcome to kANU SRK GROUP Admin Panel !!!!
+        Welcome to KANU SRK GROUP Admin Panel !!!!
       </h3>
 
     </>
